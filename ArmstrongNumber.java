e509439870c35432f79558d2cec1f3c8092a1210
Akash.ziz <akash701043@gmail.com>
2025-02-14 21:42:21 +0530

    armstrong added

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ArmstrongNumber {
     public static void main(String[] args) {
-
-    }   
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int num = in.nextInt();
+        
+        int orgNum = num;
+        int cubeSum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            cubeSum = cubeSum + (digit * digit * digit);
+            num /= 10;
+        }
+        if (cubeSum == orgNum) {
+            System.out.println(orgNum + " is an Armstrong number");
+        } else {
+            System.out.println(orgNum + " is not an Armstrong number");
+        }
+    }
 }
